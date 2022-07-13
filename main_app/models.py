4e9a6f9eb4dd37116e2c1804f3d7904fffed4b9b
Oby <obyli214@gmail.com>
2022-07-13 00:21:51 -0700

    added Pokemon model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    level = models.IntegerField()
+    description = models.TextField(max_length=250)
