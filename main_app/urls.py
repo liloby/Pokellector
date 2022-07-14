ad5e1aa5416d048af2123db0288915e350b63821
Oby <obyli214@gmail.com>
2022-07-14 00:06:05 -0700

    added animation on index page and catch a pokemon tab

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('pokemons/', views.pokemons_index, name='index'),
-    path('pokemons/<int:pokemon_id>/', views.pokemons_detail, name='detail')
+    path('pokemons/<int:pokemon_id>/', views.pokemons_detail, name='detail'),
+    path('pokemons/create/', views.PokemonCreate.as_view(), name='pokemons_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Pokemon
 
 from django.http import HttpResponse
@@ -11,9 +12,15 @@ def about(request):
     return render(request, 'about.html')
 
 def pokemons_index(request):
-    pokemons = Pokemon.objects.order_by('id')
-    return render(request, 'pokemons/index.html', { 'pokemons': pokemons })
+    pokemons = Pokemon.objects.order_by('-level')
+    latest_pokemon = Pokemon.objects.order_by('id').last
+    return render(request, 'pokemons/index.html', { 'pokemons': pokemons, 'latest_pokemon': latest_pokemon })
 
 def pokemons_detail(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
     return render(request, 'pokemons/detail.html', { 'pokemon': pokemon })
+
+class PokemonCreate(CreateView):
+    model = Pokemon
+    fields = '__all__'
+    success_url = '/pokemons/'
