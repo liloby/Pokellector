feff7ede530b1f9207a081289afc0effdd5cb640
Oby <obyli214@gmail.com>
2022-07-15 00:53:00 -0700

    added interaction models and uploaded to Admin Portal

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+INTERACTS = (
+    ('P', 'Pet'),
+    ('G', 'Groom'),
+    ('F', 'Feed'),
+
+)
+
 # Create your models here.
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +20,16 @@ class Pokemon(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pokemon_id': self.id})
+
+class Interaction(models.Model):
+    date = models.DateField()
+    interact = models.CharField(
+        max_length=1,
+        choices=INTERACTS,
+        default=INTERACTS[0][0]
+        )
+
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_interact_display()} on {self.date}"
